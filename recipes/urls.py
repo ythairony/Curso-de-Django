e6d49c37c8346647744cf0b48190f8519eccd42e
Yuri Thairony <yurithairony@hotmail.com>
2023-03-26 10:18:30 -0300

    Finalizando sessão de herança

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+#recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipes, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -12,5 +12,6 @@ def home(request):
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
