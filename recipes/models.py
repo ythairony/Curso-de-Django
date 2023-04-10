5f731bbc7f1856b91a4652e832278288e80c2f79
Yuri Thairony <yurithairony@hotmail.com>
2023-04-10 19:20:28 -0300

    andamento do curso

--- recipes/models.py
+++ recipes/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -23,6 +26,9 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
 
 
 '''
